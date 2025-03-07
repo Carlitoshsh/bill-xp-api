7681208adc77d591707086a2abdd828f1f163f4a
Carlos Gómez <caragomezher@unal.edu.co>
2025-03-07 12:05:53 -0500

    Add default route

--- api/controllers/entry.js
+++ api/controllers/entry.js
@@ -2,6 +2,11 @@ import product from './products.js'
 import invoices from './invoices.js'
 
 export default function setupControllers(app) {
+    app.get('/', (req, res) => {
+        res.send({
+            about: 'Simple API for save invoices'
+        });
+    })
     app.get('/health', (req, res) => {
         res.send({
             connected: true
